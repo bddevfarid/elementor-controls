f03218b5bd1c78bc5c21a11f8b3c46384c08d4de
Faridul Islam <73481902+bddevfarid@users.noreply.github.com>
2022-09-27 15:57:26 +0600

    Update index.php

--- index.php
+++ index.php
@@ -25,3 +25,54 @@ $this->add_control(
         'render_type' => 'template'
     ]
 );
+
+
+$this->add_group_control(
+    Group_Control_Border::get_type(),
+    [
+        'name' => 'item_border',
+        'label' => esc_html__('Border', 'elementor-addons'),
+        'fields_options' => [
+            'border' => [
+                'default' => 'solid',
+            ],
+            'width' => [
+                'default' => [
+                    'top' => '1',
+                    'right' => '1',
+                    'bottom' => '1',
+                    'left' => '1',
+                    'unit' => 'px',
+                    'isLinked' => false,
+                ],
+            ],
+            'color' => [
+                'default' => '#eaeaea',
+            ],
+        ],
+        'selector' => '{{WRAPPER}} .class-name',
+    ]
+);
+
+$this->add_group_control(
+    Group_Control_Background::get_type(),
+    [
+        'name' => 'content_background',
+        'label' => esc_html__('Background', 'pixel-gallery'),
+        'types' => ['classic', 'gradient'],
+        'exclude' => ['image'],
+        'selector' => '{{WRAPPER}}',
+        'fields_options' => [
+            'background' => [
+                'label' => esc_html__('Custom Background', 'pixel-gallery'),
+                'default' => 'gradient',
+            ],
+            'color' => [
+                'default' => '#fff',
+            ],
+            'color_b' => [
+                'default' => '#000',
+            ],
+        ],
+    ]
+);
