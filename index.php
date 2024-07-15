ce2f39ba1c83bc5e046e441820a1fff63bfc9034
Faridul Islam <73481902+bddevfarid@users.noreply.github.com>
2024-07-15 15:08:01 +0600

    Update index.php

--- index.php
+++ index.php
@@ -190,3 +190,211 @@ $this->add_group_control(
 		],
 	]
 );
+
+
+$this->add_responsive_control(
+            'image',
+            [
+                'label' => __('Image', 'ultimate-store-kit'),
+                'type' => Controls_Manager::MEDIA,
+                'default' => [
+                    'url' => Utils::get_placeholder_image_src(),
+                ],
+                'selectors' => [
+                    '{{WRAPPER}} .usk-featured-box .usk-image-wrap' => 'background-image: url("{{URL}}");',
+                ],
+                'has_sizes' => true,
+                'render_type' => 'template',
+            ]
+        );
+        $this->add_responsive_control(
+            'position',
+            [
+                'label' => esc_html__( 'Position', 'elementor' ),
+                'type' => Controls_Manager::SELECT,
+                'default' => '',
+                'separator' => 'before',
+                'options' => [
+                    '' => esc_html__( 'Default', 'elementor' ),
+                    'center center' => esc_html__( 'Center Center', 'elementor' ),
+                    'center left' => esc_html__( 'Center Left', 'elementor' ),
+                    'center right' => esc_html__( 'Center Right', 'elementor' ),
+                    'top center' => esc_html__( 'Top Center', 'elementor' ),
+                    'top left' => esc_html__( 'Top Left', 'elementor' ),
+                    'top right' => esc_html__( 'Top Right', 'elementor' ),
+                    'bottom center' => esc_html__( 'Bottom Center', 'elementor' ),
+                    'bottom left' => esc_html__( 'Bottom Left', 'elementor' ),
+                    'bottom right' => esc_html__( 'Bottom Right', 'elementor' ),
+                    'initial' => esc_html__( 'Custom', 'elementor' ),
+    
+                ],
+                'selectors' => [
+                    '{{WRAPPER}} .usk-featured-box .usk-image-wrap' => 'background-position: {{VALUE}};',
+                ],
+                'condition' => [
+                    'image[url]!' => '',
+                ],
+            ]
+        );
+
+        $this->add_responsive_control(
+            'xpos', 
+            [
+                'label' => esc_html__( 'X Position', 'elementor' ),
+                'type' => Controls_Manager::SLIDER,
+                'responsive' => true,
+                'size_units' => [ 'px', '%', 'em', 'rem', 'vw', 'custom' ],
+                'default' => [
+                    'size' => 0,
+                ],
+                'tablet_default' => [
+                    'size' => 0,
+                ],
+                'mobile_default' => [
+                    'size' => 0,
+                ],
+                'range' => [
+                    'px' => [
+                        'min' => -800,
+                        'max' => 800,
+                    ],
+                    'em' => [
+                        'min' => -100,
+                        'max' => 100,
+                    ],
+                    '%' => [
+                        'min' => -100,
+                        'max' => 100,
+                    ],
+                    'vw' => [
+                        'min' => -100,
+                        'max' => 100,
+                    ],
+                ],
+                'selectors' => [
+                    '{{WRAPPER}} .usk-featured-box .usk-image-wrap' => 'background-position: {{SIZE}}{{UNIT}} {{ypos.SIZE}}{{ypos.UNIT}}',
+                ],
+                'condition' => [
+                    'position' => [ 'initial' ],
+                    'image[url]!' => '',
+                ],
+                'required' => true,
+            ]
+        );
+		
+        $this->add_responsive_control(
+            'ypos', 
+            [
+                'label' => esc_html__( 'Y Position', 'elementor' ),
+                'type' => Controls_Manager::SLIDER,
+                'responsive' => true,
+                'size_units' => [ 'px', '%', 'em', 'rem', 'vh', 'custom' ],
+                'default' => [
+                    'size' => 0,
+                ],
+                'tablet_default' => [
+                    'size' => 0,
+                ],
+                'mobile_default' => [
+                    'size' => 0,
+                ],
+                'range' => [
+                    'px' => [
+                        'min' => -800,
+                        'max' => 800,
+                    ],
+                    'em' => [
+                        'min' => -100,
+                        'max' => 100,
+                    ],
+                    '%' => [
+                        'min' => -100,
+                        'max' => 100,
+                    ],
+                    'vh' => [
+                        'min' => -100,
+                        'max' => 100,
+                    ],
+                ],
+                'selectors' => [
+                    '{{WRAPPER}} .usk-featured-box .usk-image-wrap' => 'background-position: {{xpos.SIZE}}{{xpos.UNIT}} {{SIZE}}{{UNIT}}',
+                ],
+                'condition' => [
+                    'position' => [ 'initial' ],
+                    'image[url]!' => '',
+                ],
+                'required' => true,
+            ]
+        );
+
+        $this->add_responsive_control(
+            'repeat', 
+            [
+                'label' => esc_html_x( 'Repeat', 'Background Control', 'elementor' ),
+                'type' => Controls_Manager::SELECT,
+                'default' => '',
+                'options' => [
+                    '' => esc_html__( 'Default', 'elementor' ),
+                    'no-repeat' => esc_html__( 'No-repeat', 'elementor' ),
+                    'repeat' => esc_html__( 'Repeat', 'elementor' ),
+                    'repeat-x' => esc_html__( 'Repeat-x', 'elementor' ),
+                    'repeat-y' => esc_html__( 'Repeat-y', 'elementor' ),
+                ],
+                'selectors' => [
+                    '{{WRAPPER}} .usk-featured-box .usk-image-wrap' => 'background-repeat: {{VALUE}};',
+                ],
+                'condition' => [
+                    'image[url]!' => '',
+                ],
+            ]
+        );
+
+        $this->add_responsive_control(
+            'size', 
+            [
+                'label' => esc_html__( 'Display Size', 'elementor' ),
+                'type' => Controls_Manager::SELECT,
+                'default' => '',
+                'options' => [
+                    '' => esc_html__( 'Default', 'elementor' ),
+                    'auto' => esc_html__( 'Auto', 'elementor' ),
+                    'cover' => esc_html__( 'Cover', 'elementor' ),
+                    'contain' => esc_html__( 'Contain', 'elementor' ),
+                    'initial' => esc_html__( 'Custom', 'elementor' ),
+                ],
+                'selectors' => [
+                    '{{WRAPPER}} .usk-featured-box .usk-image-wrap' => 'background-size: {{VALUE}};',
+                ],
+                'condition' => [
+                    'image[url]!' => '',
+                ],
+            ]
+        );
+
+        $this->add_responsive_control(
+            'bg_width', 
+            [
+                'label' => esc_html__( 'Width', 'elementor' ),
+                'type' => Controls_Manager::SLIDER,
+                'responsive' => true,
+                'size_units' => [ 'px', '%', 'em', 'rem', 'vw', 'custom' ],
+                'range' => [
+                    'px' => [
+                        'max' => 1000,
+                    ],
+                ],
+                'default' => [
+                    'size' => 100,
+                    'unit' => '%',
+                ],
+                'required' => true,
+                'selectors' => [
+                    '{{WRAPPER}} .usk-featured-box .usk-image-wrap' => 'background-size: {{SIZE}}{{UNIT}} auto',
+    
+                ],
+                'condition' => [
+                    'size' => [ 'initial' ],
+                    'image[url]!' => '',
+                ],
+            ]
+        );
